b2b9743188296b54f34f80d3f5f7b3e39a8300f9
liugr <13466957481@163.com>
2019-04-17 19:32:35 +0800

    cs

--- src/page/document/index.vue
+++ src/page/document/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <template>
-      <el-table v-for="(item,index) in tableData" :key="index" height="250" border style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180">
+      <el-table :data="tableData" height="830px" border stripe>
+        <el-table-column prop="date" label="日期">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180">
+        <el-table-column prop="title" label="标题">
         </el-table-column>
-        <el-table-column prop="address" label="地址">
+        <el-table-column prop="author" label="作者">
+        </el-table-column>
+        <el-table-column prop="icon" label="图标">
+          <template slot-scope="scope">
+            <img :src="scope.row.icon" width="50px" height="50px">
+          </template>
+        </el-table-column>
+        <el-table-column>
+          <template slot-scope="scope">
+            <el-button type="danger" @click="deleted(scope.row)">删除</el-button>
+            <el-button type="success" @click="changed(scope.row)">修改</el-button>
+          </template>
         </el-table-column>
       </el-table>
     </template>
@@ -26,13 +37,54 @@
     methods: {
       initDate() {
         axios.get("http://mockjs.com/api/posts", {
-        }).then(function(data) {
-          console.log(data.data.posts)
-          // this.title = data.data
-        }).catch(function(err) {
-          // console.log(err)
+        }).then(res => {
+          this.tableData = res.data.posts
+        })
+      },
+
+      deleted(row) {
+        console.log(row);
+        this.$confirm(`是否删除"${row.title}"？`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          axios.post('http://mockjs.com/api/content').then(res => {
+            this.tableData.splice(row, 1);
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            });
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        })
+      },
+      changed(row) {
+        this.$confirm(`是否修改"${row.title}"？`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          axios.post('http://mockjs.com/api/content').then(res => {
+            alert(111)
+            // this.$message({
+            //   type: 'success',
+            //   message: '修改成功!'
+            // });
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消修改'
+          });
         })
       }
+    },
+    mounted() {
     }
   }
 </script>
